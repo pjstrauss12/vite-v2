5fc3a574c8ad9fc5906a62991e0e427857f9a409
pjstrauss12 <parkerstrauss21@gmail.com>
2023-02-09 12:48:01 -0500

    Card works!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1>{{ msg }}</h1>
 
   <div class="fullcard">
     <h1>Steel Vengeance</h1>
@@ -15,20 +14,6 @@
 
   <button class="del" v-on:click="deleter">Remove Last</button>
 
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <script setup>
